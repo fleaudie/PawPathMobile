0e9fdb2986d8cd4c247ec6fddd71f78e93f076ad
fleaudie <hiedixx@gmail.com>
2023-11-10 14:22:57 +0300

    update

---
+++ app/src/main/java/com/fleaudie/pawpath/HealthService.kt
@@ -0,0 +1,11 @@
+package com.fleaudie.pawpath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HealthService : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.health_service)
+    }
+}
